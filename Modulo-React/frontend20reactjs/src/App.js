30747aaa348c0045f40453ac167e610382394986
Fabio Lima <84404364+LimaDev-Max@users.noreply.github.com>
2022-03-11 17:51:21 -0300

    React js - Danki Code

--- Modulo-React/frontend20reactjs/src/App.js
+++ Modulo-React/frontend20reactjs/src/App.js
@@ -7,25 +7,25 @@ function App() {
   // Declare uma nova variável de state, a qual chamaremos de "count"
 
   const [count, setCount] = useState(0);
-
-
-
+  const [nome, setNome] = useState("Joao")
+  const [numeros, setarNumeros] = useState([1,2,3,4,5,6])
+  const [obj, setObj] = useState({nome: 'gui',idade:28});
   return (
 
     <div>
-
-      <p>You clicked {count} times</p>
-
-      <button onClick={() => setCount(count + 1)}>
-
+      {
+        numeros.map(function(val){
+          return(
+            <p>{val}</p>
+          )
+        })
+      }
+      
+      <button onClick={() => setNome("Fabio")}>
         Click me
-
       </button>
-
     </div>
-
   );
-
 }
 
 export default App;
