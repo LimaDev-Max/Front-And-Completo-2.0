e12cec0b73cb2c281067651212761007843bd9fb
Fabio Lima <84404364+LimaDev-Max@users.noreply.github.com>
2022-03-12 15:01:38 -0300

    Update App.js

--- Modulo-React/frontend20reactjs/src/App.js
+++ Modulo-React/frontend20reactjs/src/App.js
@@ -2,19 +2,29 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [hora, setHora] = useState(12);
-  const [minutos, setMinutos] = useState(20);
-  
+  const [minuto, setMinuto] = useState(59);
+  const [segundo, setSegundo] = useState(0)
+
   useEffect(() => {
     const interval = setInterval(() => {
-
+     
+      setSegundo(segundo+1);
+      if(segundo == 59){
+        setSegundo(0);
+        setMinuto(hora+1);
+        if(minuto == 59){
+          setMinuto(0);
+          setHora(hora+1)
+        }
+      }
     }, 1000);
     
     return () => clearInterval(interval);
-  }, []);
+  },);
 
   return (
     <div>
-      <h2>{hora}:{minutos}</h2>
+      <h2 style={{textAlign:'center'}}>{hora}:{minuto}:{segundo}</h2>
     </div>
   );
 }
